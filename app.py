36f48730a54f56505d6f812c7eb50aaf34498f62
Marc Collado <MarcCollado@users.noreply.github.com>
2017-08-07 23:00:37 +0200

    app.py code calls and executes the rest of the scripts.

---
+++ app.py
@@ -0,0 +1,16 @@
+import audit
+import compress
+import to_csv
+# import to_sql
+
+PATH = "../../_data"
+FILE = "bcn.min.osm"
+OSM_PATH = "{}/{}".format(PATH, FILE)
+
+if __name__ == "__main__":
+    with open("{}/{}".format(PATH, FILE), "r") as f:
+        # audit.quick_print(f)
+        # audit.audit_nodes(f)
+        # audit.audit_ways(f)
+        pass
+    to_csv.process_map(OSM_PATH, validate=False)
