527b07a8e3ad83eb8ae531ca1e6d3c9740371e6b
Marc Collado <MarcCollado@users.noreply.github.com>
2017-08-20 18:14:51 +0200

    Prepare project for submission * Modify data path * Add time() between functions to improve stability * Add to_sql() function to the script

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import audit
 import to_csv
-# import to_sql
+import to_sql
+import time
 
-PATH = "../../_data"
-FILE = "bcn.min.osm"
+PATH = "data/"
+FILE = "bcn_sample.osm"
 OSM_PATH = "{}/{}".format(PATH, FILE)
 
 if __name__ == "__main__":
@@ -14,3 +15,5 @@ if __name__ == "__main__":
         # audit.audit_ways(f)
         pass
     to_csv.process_map(OSM_PATH, validate=False)
+    time.sleep(5)
+    to_sql.main()
