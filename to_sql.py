e8c8ee27550860998d7724e4f936a4900b366e5c
Marc Collado <MarcCollado@users.noreply.github.com>
2017-08-20 18:19:27 +0200

    * Add main() wrapper to be called from app.py

--- to_sql.py
+++ to_sql.py
@@ -2,115 +2,112 @@
 import sqlite3
 import csv
 
-sqlite_file = 'bcn.db'
-conn = sqlite3.connect(sqlite_file)
-conn.text_factory = str
 
-cur = conn.cursor()
+def main():
+    sqlite_file = 'data/bcn.db'
+    conn = sqlite3.connect(sqlite_file)
+    conn.text_factory = str
 
-cur.execute('''
-    CREATE TABLE node (
-        id INTEGER PRIMARY KEY,
-        lat REAL,
-        lon REAL,
-        user TEXT,
-        uid INTEGER,
-        version TEXT,
-        changeset INTEGER,
-        timestamp TEXT
-        );
-        ''')
+    cur = conn.cursor()
 
-conn.commit()
+    cur.execute('''
+        CREATE TABLE node (
+            id INTEGER PRIMARY KEY,
+            lat REAL,
+            lon REAL,
+            user TEXT,
+            uid INTEGER,
+            version TEXT,
+            changeset INTEGER,
+            timestamp TEXT
+            );
+            ''')
 
-with open('output/node.csv', 'rb') as f:
-    dr = csv.DictReader(f)
-    to_db = [(i['id'], i['lat'], i['lon'], i['user'].decode('utf-8'), i['uid'], i['version'], i['changeset'], i['timestamp']) for i in dr]
+    conn.commit()
 
-cur.executemany('INSERT INTO node(id, lat, lon, user, uid, version, changeset, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?);', to_db)
+    with open('data/node.csv', 'rb') as f:
+        dr = csv.DictReader(f)
+        to_db = [(i['id'], i['lat'], i['lon'], i['user'].decode('utf-8'), i['uid'], i['version'], i['changeset'], i['timestamp']) for i in dr]
 
-conn.commit()
+    cur.executemany('INSERT INTO node(id, lat, lon, user, uid, version, changeset, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?);', to_db)
 
+    conn.commit()
 
-cur.execute('''
-    CREATE TABLE node_tags (
-        id INTEGER REFERENCES node (id),
-        key TEXT,
-        value TEXT,
-        type TEXT
-        );
-        ''')
+    cur.execute('''
+        CREATE TABLE node_tags (
+            id INTEGER REFERENCES node (id),
+            key TEXT,
+            value TEXT,
+            type TEXT
+            );
+            ''')
 
-conn.commit()
+    conn.commit()
 
-with open('output/node_tags.csv', 'rb') as f:
-    dr = csv.DictReader(f)
-    to_db = [(i['id'], i['key'], i['value'].decode('utf-8'), i['type']) for i in dr]
+    with open('data/node_tags.csv', 'rb') as f:
+        dr = csv.DictReader(f)
+        to_db = [(i['id'], i['key'], i['value'].decode('utf-8'), i['type']) for i in dr]
 
-cur.executemany('INSERT INTO node_tags(id, key, value, type) VALUES (?, ?, ?, ?);', to_db)
+    cur.executemany('INSERT INTO node_tags(id, key, value, type) VALUES (?, ?, ?, ?);', to_db)
 
-conn.commit()
+    conn.commit()
 
+    cur.execute('''
+        CREATE TABLE way (
+            id INTEGER PRIMARY KEY,
+            user TEXT,
+            uid INTEGER,
+            version TEXT,
+            changeset INTEGER,
+            timestamp TEXT
+            );
+            ''')
 
-cur.execute('''
-    CREATE TABLE way (
-        id INTEGER PRIMARY KEY,
-        user TEXT,
-        uid INTEGER,
-        version TEXT,
-        changeset INTEGER,
-        timestamp TEXT
-        );
-        ''')
+    conn.commit()
 
-conn.commit()
+    with open('data/way.csv', 'rb') as f:
+        dr = csv.DictReader(f)
+        to_db = [(i['id'], i['user'].decode('utf-8'), i['uid'], i['version'], i['changeset'], i['timestamp']) for i in dr]
 
-with open('output/way.csv', 'rb') as f:
-    dr = csv.DictReader(f)
-    to_db = [(i['id'], i['user'].decode('utf-8'), i['uid'], i['version'], i['changeset'], i['timestamp']) for i in dr]
+    cur.executemany('INSERT INTO way(id, user, uid, version, changeset, timestamp) VALUES (?, ?, ?, ?, ?, ?);', to_db)
 
-cur.executemany('INSERT INTO way(id, user, uid, version, changeset, timestamp) VALUES (?, ?, ?, ?, ?, ?);', to_db)
+    conn.commit()
 
-conn.commit()
+    cur.execute('''
+        CREATE TABLE way_nodes (
+            id INTEGER REFERENCES way (id),
+            node_id INTEGER,
+            position INTEGER
+            );
+            ''')
 
+    conn.commit()
 
-cur.execute('''
-    CREATE TABLE way_nodes (
-        id INTEGER REFERENCES way (id),
-        node_id INTEGER,
-        position INTEGER
-        );
-        ''')
+    with open('data/way_nodes.csv', 'rb') as f:
+        dr = csv.DictReader(f)
+        to_db = [(i['id'], i['node_id'], i['position']) for i in dr]
 
-conn.commit()
+    cur.executemany('INSERT INTO way_nodes(id, node_id, position) VALUES (?, ?, ?);', to_db)
 
-with open('output/way_nodes.csv', 'rb') as f:
-    dr = csv.DictReader(f)
-    to_db = [(i['id'], i['node_id'], i['position']) for i in dr]
+    conn.commit()
 
-cur.executemany('INSERT INTO way_nodes(id, node_id, position) VALUES (?, ?, ?);', to_db)
+    cur.execute('''
+        CREATE TABLE way_tags (
+            id INTEGER REFERENCES way (id),
+            key TEXT,
+            value TEXT,
+            type TEXT
+            );
+            ''')
 
-conn.commit()
+    conn.commit()
 
+    with open('data/way_tags.csv', 'rb') as f:
+        dr = csv.DictReader(f)
+        to_db = [(i['id'], i['key'], i['value'].decode('utf-8'), i['type']) for i in dr]
 
-cur.execute('''
-    CREATE TABLE way_tags (
-        id INTEGER REFERENCES way (id),
-        key TEXT,
-        value TEXT,
-        type TEXT
-        );
-        ''')
+    cur.executemany('INSERT INTO way_tags(id, key, value, type) VALUES (?, ?, ?, ?);', to_db)
 
-conn.commit()
+    conn.commit()
 
-with open('output/way_tags.csv', 'rb') as f:
-    dr = csv.DictReader(f)
-    to_db = [(i['id'], i['key'], i['value'].decode('utf-8'), i['type']) for i in dr]
-
-cur.executemany('INSERT INTO way_tags(id, key, value, type) VALUES (?, ?, ?, ?);', to_db)
-
-conn.commit()
-
-
-conn.close()
+    conn.close()
